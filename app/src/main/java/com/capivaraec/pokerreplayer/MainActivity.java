c1b8adffcff9dd5696cb306ff176c1de7a5c488f
Capivara Esporte Clube <capivaraec@gmail.com>
2015-11-20 00:46:11 -0200

    Integração SDK Dropbox

--- app/src/main/java/com/capivaraec/pokerreplayer/MainActivity.java
+++ app/src/main/java/com/capivaraec/pokerreplayer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.capivaraec.pokerreplayer;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import android.widget.Button;
 
 import com.capivaraec.pokerreplayer.components.Player;
 import com.capivaraec.pokerreplayer.filebrowser.FileBrowserActivity;
+import com.dropbox.chooser.android.DbxChooser;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String PREFS_NAME = "preferences";
     private static final String FILE_PATH = "file_path";
+    private static final int DBX_CHOOSER_REQUEST = 0;
+    private DbxChooser mChooser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +40,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openFileBrowser(View v) {
-        Intent intent = new Intent(this, FileBrowserActivity.class);
-        startActivityForResult(intent, 1);
+        //TODO: exibir alert com as opções de chooser (dispositivo, Dropbox, Google Drive ou OneDrive)
+        openDropbox();
+
+//        Intent intent = new Intent(this, FileBrowserActivity.class);
+//        startActivityForResult(intent, 1);
+    }
+
+    private void openDropbox() {
+        if (mChooser == null) {
+            mChooser = new DbxChooser("xwctj62gvp3l598");
+        }
+        mChooser.forResultType(DbxChooser.ResultType.FILE_CONTENT).launch(MainActivity.this, DBX_CHOOSER_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == DBX_CHOOSER_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+                DbxChooser.Result result = new DbxChooser.Result(data);
+                System.out.println(result.getLink()); //endereço do arquivo no dispositivo
+
+                // Handle the result
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
