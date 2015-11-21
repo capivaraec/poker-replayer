b2afd278e174d5313761278be02fa2d6f96d73bb
Capivara Esporte Clube <capivaraec@gmail.com>
2015-11-21 12:21:25 -0200

    Criação bottom sheet

--- app/src/main/java/com/capivaraec/pokerreplayer/MainActivity.java
+++ app/src/main/java/com/capivaraec/pokerreplayer/MainActivity.java
@@ -2,15 +2,18 @@ package com.capivaraec.pokerreplayer;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.capivaraec.pokerreplayer.components.Player;
 import com.capivaraec.pokerreplayer.filebrowser.FileBrowserActivity;
@@ -26,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String FILE_PATH = "file_path";
     private static final int DBX_CHOOSER_REQUEST = 0;
     private DbxChooser mChooser;
+    private Dialog mBottomSheetDialog;
+
+    private Player[] players;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,21 +39,46 @@ public class MainActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        setBottomSheet();
 
-        Player player1 = (Player) findViewById(R.id.player_1);
-        player1.setName("Marcelo_CAP");
-        player1.setStack(1200);
+        setPlayers();
     }
 
-    public void openFileBrowser(View v) {
-        //TODO: exibir alert com as opções de chooser (dispositivo, Dropbox, Google Drive ou OneDrive)
-        openDropbox();
-
-//        Intent intent = new Intent(this, FileBrowserActivity.class);
-//        startActivityForResult(intent, 1);
+    private void setBottomSheet() {
+        mBottomSheetDialog = new Dialog(MainActivity.this, R.style.MaterialDialogSheet);
+        mBottomSheetDialog.setContentView(R.layout.bottom_sheet);
+        mBottomSheetDialog.setCancelable(true);
+        mBottomSheetDialog.getWindow().setLayout(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        mBottomSheetDialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 
-    private void openDropbox() {
+    private void setPlayers() {
+        //TODO: chamar este método apenas após carregar o torneio (se for 6-max, carregar somente 6 jogadores)
+        players = new Player[10];
+
+        for(int x = 1; x <= 10; x++) {
+            String playerId = "player_" + x;
+            int resID = getResources().getIdentifier(playerId, "id", getPackageName());
+            players[x - 1] = (Player) findViewById(resID);
+        }
+    }
+
+    public void openBottomSheet(View v) {
+        mBottomSheetDialog.show();
+    }
+
+    public void closeBottomSheet(View v) {
+        mBottomSheetDialog.hide();
+    }
+
+    public void openDeviceBrowser(View v) {
+        closeBottomSheet(null);
+        Intent intent = new Intent(this, FileBrowserActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    public void openDropboxBrowser(View v) {
+        closeBottomSheet(null);
         if (mChooser == null) {
             mChooser = new DbxChooser("xwctj62gvp3l598");
         }
